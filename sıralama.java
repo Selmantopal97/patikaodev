495a1cc197b7f256b9f9f845cdbadefecd15804d
Selmantopal97 <setpa97@hotmail.com>
2022-06-08 20:10:47 +0300

    küçükten büyüğe sıralandı

--- sıralama.java
+++ sıralama.java
@@ -14,25 +14,25 @@ public class sıralama {
         System.out.print("üçüncü sayıyı girin: ");
         c=sayi.nextInt();
 
-        if(a>b && a>c){
-            if(b>c){
-                System.out.println("a > b > c");
+        if(a<b && a<c){
+            if(b<c){
+                System.out.println("a < b < c");
             }else{
-                System.out.println("a > c > b");
+                System.out.println("a < c < b");
             }
         }
-            if(b>a && b>c){
-                if(a>c){
-                    System.out.println("b > a > c"); 
+            if(b<a && b<c){
+                if(a<c){
+                    System.out.println("b < a < c"); 
                 }else{
-                    System.out.println("b > c > a");
+                    System.out.println("b < c < a");
                 }
             }
-            if(c>a && c>b){
-                if(a>b){
-                    System.out.println("c > a > b");
+            if(c<a && c<b){
+                if(a<b){
+                    System.out.println("c < a < b");
                 }else{
-                    System.out.println("c > b > a");
+                    System.out.println("c < b < a");
                 }
             }
     }
